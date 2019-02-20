8b458294fb679a1b45ff8a675b4a154f5493d566
marhatha <puneet.marhatha@gmail.com>
2019-02-19 18:26:49 -0600

    Update image.php

--- image.php
+++ image.php
@@ -6,7 +6,7 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'green') {
-  $color = imagecolorallocate($im, 0, 255, 0);
+  //$color = imagecolorallocate($im, 0, 255, 0);
 } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
 } else {
